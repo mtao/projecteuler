11430b25e9e3a545d39c51715d47c2f53229cf2a
Michael Tao <mtao@dgp.toronto.edu>
2013-05-03 09:30:22 -0400

    how embarassing p1 scala was wrong :(

--- scala/p1/P1.scala
+++ scala/p1/P1.scala
@@ -1,6 +1,6 @@
 object P1 extends App{
     def triangleNum(x: Int): Int = {
-        x*(x-1)/2
+        x*(x+1)/2
     }
     def numOccurences(x: Int, max: Int): Int = {
         (max/x).toInt
@@ -9,7 +9,7 @@ object P1 extends App{
         x * triangleNum(numOccurences(x,max))
     }
     def sumOfMultiplesTwo(x: Int, y: Int, max: Int): Int =  {
-        sumOfMultiples(x,max) + sumOfMultiples(y,max) - sumOfMultiples(x*y, max)
+        sumOfMultiples(x,max-1) + sumOfMultiples(y,max-1) - sumOfMultiples(x*y, max-1)
     }
     override def main(args: Array[String]) {
         println(sumOfMultiplesTwo(3,5,1000))
